e884eff22856be52a545d0c6c2e3d421b31af873
Blaine Andreoli <blaineandreoli@ip0af508c9.int.uvm.edu>
2017-10-11 15:15:20 -0400

    Firebase configuration

--- eatin/AppDelegate.swift
+++ eatin/AppDelegate.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    override init() {
+        FirebaseApp.configure()
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
         return true
     }
 
---
+++ eatin/Controller/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  eatin
+//
+//  Created by Blaine Andreoli on 10/11/17.
+//  Copyright © 2017 CS275. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    let ref = Database.database().reference()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+    
+    @objc
+    func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+}
+
