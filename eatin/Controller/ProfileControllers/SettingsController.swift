ed4ff063473a7308f0b328cc4dc2379ce06efcec
Grant Slattery <grantslattery@gmail.com>
2017-11-01 20:11:05 -0400

    Settings logout button added

--- eatin/Controller/ProfileControllers/SettingsController.swift
+++ eatin/Controller/ProfileControllers/SettingsController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
 
 class SettingsController: UIViewController {
     
@@ -21,6 +22,16 @@ class SettingsController: UIViewController {
         return deleteButton
     }()
     
+    let logoutButton: UIButton = {
+        let logoutButton = UIButton()
+        logoutButton.backgroundColor = UIColor.red
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.setTitle("Logout", for: .normal)
+        logoutButton.setTitleColor(UIColor.white, for: .normal)
+        logoutButton.addTarget(self, action: #selector(logoutButtonAction), for: .touchUpInside)
+        return logoutButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,15 +40,22 @@ class SettingsController: UIViewController {
         view.backgroundColor = UIColor(r: 255, g: 255, b: 255)
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Return", style: .plain, target: self, action: #selector(handleReturn))
-        
+      
+        self.view.addSubview(logoutButton)
         self.view.addSubview(deleteButton)
+
         
         addButtons()
     }
     
     func addButtons() {
+        logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoutButton.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 12).isActive = true
+        logoutButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        logoutButton.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        
         deleteButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        deleteButton.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 12).isActive = true
+        deleteButton.topAnchor.constraint(equalTo: logoutButton.bottomAnchor, constant: 12).isActive = true
         deleteButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         deleteButton.heightAnchor.constraint(equalToConstant: 75).isActive = true
     }
@@ -52,6 +70,15 @@ class SettingsController: UIViewController {
     func deleteButtonAction() {
         let alert = UIAlertController(title: "Account Deletion", message: "Are you sure you want to delete your account?", preferredStyle: .alert)
         alert.addAction(okAction)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @objc
+    func logoutButtonAction() {
+        let alert = UIAlertController(title: "Account Logout", message: "Are you sure you want to logout of your account?", preferredStyle: .alert)
+        alert.addAction(yesAction)
         alert.addAction(UIAlertAction(title: "Cancel", style: .default))
         self.present(alert, animated: true, completion: nil)
         
@@ -78,6 +105,24 @@ class SettingsController: UIViewController {
         }
         
         let introController = IntroController()
+        self.present(introController, animated: true, completion: nil)
+    }
+    
+    lazy var yesAction = UIAlertAction(title: "Yes", style: UIAlertActionStyle.default) {
+        UIAlertAction in
+        do {
+            try Auth.auth().signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
+        
+        if(FBSDKAccessToken.current() != nil) {
+            let loginManager = FBSDKLoginManager()
+            loginManager.logOut()
+        }
+        
+        let introController = IntroController()
+        
         self.present(introController, animated: true, completion: nil)
     }
     
